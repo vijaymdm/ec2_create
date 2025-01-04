03300bc609780805af8d50f85143488e2d536fe9
vijaymdm <vijaymdm@gmail.com>
2025-01-04 08:04:09 +0530

    first_commit

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "App" {
-ami = "ami-08a52ddb321b32a8c"
-instance_type = "t2.micro"
+  ami           = "ami-08a52ddb321b32a8c"
+  instance_type = "t2.micro"
 
-tags = {
-  name = "MyWorld"
-}
-  
+  tags = {
+    Name             = "Lambda_Scheduler"
+    Lambda_Scheduler = "office_hours"
+  }
+
 }
